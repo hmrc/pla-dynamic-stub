c96f09dd434e0ad338f752d13a3f817b506ea51d
geethumathew <geethuelizebeth@gmail.com>
2025-04-07 12:49:21 +0100

    DL-15861:Dependency Upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,14 +3,14 @@ import sbt._
 object AppDependencies {
   import play.sbt.PlayImport._
 
-  private val bootstrapPlayVersion = "9.7.0"
+  private val bootstrapPlayVersion = "9.11.0"
   private val hmrcMongoVersion = "2.4.0"
 
   val compile = Seq(
     ws,
     "uk.gov.hmrc"         %% "bootstrap-backend-play-30" % bootstrapPlayVersion,
     "uk.gov.hmrc.mongo"   %% "hmrc-mongo-play-30"        % hmrcMongoVersion,
-    "uk.gov.hmrc"         %% "stub-data-generator"       % "1.1.0"
+    "uk.gov.hmrc"         %% "stub-data-generator"       % "1.4.0"
   )
 
   trait TestDependencies {
@@ -22,7 +22,7 @@ object AppDependencies {
     def apply() = new TestDependencies {
       override lazy val test = Seq(
         "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapPlayVersion % scope,
-        "uk.gov.hmrc"             %% "domain-play-30"             % "10.0.0"             % scope,
+        "uk.gov.hmrc"             %% "domain-play-30"             % "11.0.0"             % scope,
         "org.scalatestplus"       %% "scalatestplus-mockito"      % "1.0.0-M2"          % scope,
         "org.scalatestplus"       %% "scalatestplus-scalacheck"   % "3.1.0.0-RC2"       % scope,
         "org.scalacheck"          %% "scalacheck"                 % "1.17.0"            % scope,
