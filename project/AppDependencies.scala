9e3c81cf59854741073b9b03272224b1cad7e7ca
Eoghan-Spillane-Accenture <201935567+Eoghan-Spillane-Accenture@users.noreply.github.com>
2026-04-30 10:25:34 +0100

    DL-19096: Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,12 +1,12 @@
-import sbt._
+import sbt.*
 
 object AppDependencies {
-  import play.sbt.PlayImport._
+  import play.sbt.PlayImport.*
 
-  private val bootstrapPlayVersion = "9.19.0"
-  private val hmrcMongoVersion     = "2.10.0"
+  private val bootstrapPlayVersion = "10.7.0"
+  private val hmrcMongoVersion     = "2.12.0"
 
-  val compile = Seq(
+  val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % bootstrapPlayVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"        % hmrcMongoVersion,
@@ -15,18 +15,18 @@ object AppDependencies {
 
   trait TestDependencies {
     lazy val scope: String       = "test"
-    lazy val test: Seq[ModuleID] = ???
+    val test: Seq[ModuleID]
   }
 
   object Test {
 
-    def apply() = new TestDependencies {
-      override lazy val test = Seq(
+    def apply(): Seq[ModuleID] = new TestDependencies {
+      override lazy val test: Seq[ModuleID] = Seq(
         "uk.gov.hmrc"       %% "bootstrap-test-play-30"   % bootstrapPlayVersion % scope,
         "uk.gov.hmrc"       %% "domain-play-30"           % "11.0.0"             % scope,
-        "org.scalatestplus" %% "scalatestplus-mockito"    % "1.0.0-M2"           % scope,
-        "org.scalatestplus" %% "scalatestplus-scalacheck" % "3.1.0.0-RC2"        % scope,
-        "org.scalacheck"    %% "scalacheck"               % "1.17.0"             % scope,
+        "org.scalatestplus" %% "mockito-5-23"    % "3.2.20.0"           % scope,
+        "org.scalatestplus" %% "scalacheck-1-18" % "3.2.19.0"        % scope,
+        "org.scalacheck"    %% "scalacheck"               % "1.19.0"             % scope,
         "io.chrisdavenport" %% "cats-scalacheck"          % "0.3.2"              % scope
       )
     }.test
